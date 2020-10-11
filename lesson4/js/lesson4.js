bdc39b42b6a2ccaf6f024ea5b05e317b41ab3eff
erimiyagi <miy20001@byui.edu>
2020-10-11 11:01:44 +0900

    l4

---
+++ lesson4/js/lesson4.js
@@ -0,0 +1,14 @@
+function toggleMenu(){
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function updateBox(){
+    let lastUpdate = "Last Updated: " + document.lastModified;
+    let date = new Date().getFullYear();
+
+    document.getElementById('year').innerHTML = date;
+    document.getElementById('updated').innerHTML = lastUpdate;
+}
+
+document.getElementById('lastModified').innerHTML = `Last Updated: ${document.lastModified}`;
