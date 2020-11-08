0008580bf2b35ec6e0cb68d23e3d8acdf7bd26da
erimiyagi <miy20001@byui.edu>
2020-11-08 20:49:26 +0900

    commit

---
+++ lesson6/js/lesson6.js
@@ -0,0 +1,24 @@
+function toggleMenu(){
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function updateBox(){
+    let lastUpdate = "Last Updated: " + document.lastModified;
+    let date = new Date().getFullYear();
+
+    document.getElementById('year').innerHTML = date;
+    document.getElementById('updated').innerHTML = lastUpdate;
+}
+
+document.getElementById('lastModified').innerHTML = `Last Updated: ${document.lastModified}`;
+
+WebFont.load({
+    google: {
+      families: [
+         'fontfamilynameofyourchoice'
+      ]
+    }
+  });
+
+
